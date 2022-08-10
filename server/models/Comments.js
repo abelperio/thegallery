a5e5a9f9f4424a6a0a54106cacf5d6ff3580ad69
Alessia Belperio <abelperio.98@gmail.com>
2022-08-10 21:14:41 +0930

    comment model

---
+++ server/models/Comments.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema ({
+    comment:{type: String, required: true},
+    commentBy: User,
+    createdAt: {type: Date, default: Date.now },
+})
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+const handleError = (err) => console.error(err);
+
+Comment.create(
+    {
+      comment: 'i like this'
+    },
+    (err) => (err ? handleError(err) : console.log('Created new document'))
+  );
+
+
+
+module.exports = Comment;
