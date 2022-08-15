98c0d09c22b2635a1b581f9a4593a9a12ec89e47
Alessia Belperio <abelperio.98@gmail.com>
2022-08-15 17:28:46 +0930

    preparing models for schema work

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema ({
     comment:{type: String, required: true},
-    commentBy: User,
     createdAt: {type: Date, default: Date.now },
 })
 
--- server/models/Piece.js
+++ server/models/Piece.js
@@ -2,24 +2,13 @@ const mongoose = require('mongoose');
 
 const pieceSchema = new mongoose.Schema ({
     name: {type: String, required: true},
-    // image: (??),
-    artist: User,
-    bio: string,
-    price: string,
+    image: {type: String},
+    bio: {type: String},
+    price: {type: Number},
 })
 
-const Piece = mongoose.model('Piece', commentSchema);
+const Piece = mongoose.model('Piece', pieceSchema);
 
 const handleError = (err) => console.error(err);
-
-Piece.create(
-    {
-    name: 'Jerry the jester',
-    artist: 'van gogh',
-    bio: 'this is a test',
-    price: '$40',
-    },
-    (err) => (err ? handleError(err) : console.log('Created new document'))
-  );
 
 module.exports = Piece;
