ed432fa345e3dc44338a5504265462003b93d97a
Alessia Belperio <abelperio.98@gmail.com>
2022-08-17 20:05:59 +0930

    continuing work on seeds and mutation implementation

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -9,3 +9,32 @@ query Me {
   }
 }
 `;
+
+export const QUERY_USER= gql`
+query User {
+  users {
+    _id
+    username
+    email
+    password
+    artstyle
+  }
+}
+`;
+
+export const QUERY_PIECE= gql`
+query Piece {
+  pieces {
+    _id
+    name
+    image
+    bio
+    price
+    createdAt
+    comments {
+      commentText
+      createdAt
+    }
+  }
+}
+`;
