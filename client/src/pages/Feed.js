f28a6fea2e91d4386138c30b005348b730d5c3ef
Alessia Belperio <abelperio.98@gmail.com>
2022-08-20 08:25:05 +0930

    feed react

---
+++ client/src/pages/Feed.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const PieceFeed = ({pieces, title }) => {
+  if (!pieces._id) {
+    return <h3>No Pieces Yet</h3>;
+  }
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      {pieces &&
+        pieces.map((piece) => (
+          <div key={piece._id} className="pieceid">
+            <h4 className="title">
+              {piece.user.username} <br />
+              <span style={{ fontSize: '1rem' }}>
+                created {piece.name}
+              </span>
+            </h4>
+            <div className="image">
+              <p>{piece.image}</p>
+            </div>
+            <div className="body">
+              <p>{piece.bio}</p>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default PieceFeed;
